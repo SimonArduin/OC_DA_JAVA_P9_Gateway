5d7e468a076c9015a4dec781e4ff39b4539e75d1
SimonArduin <125870016+SimonArduin@users.noreply.github.com>
2024-02-23 18:54:52 +0100

    fixed jakarta.servlet.Filter not found

--- gateway/src/main/java/com/medilabo/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/medilabo/gateway/SecurityConfig.java
@@ -8,9 +8,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
