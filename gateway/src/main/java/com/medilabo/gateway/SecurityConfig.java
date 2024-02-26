b1d497be8636f7c32d290b8a2a3e84a08123efa3
SimonArduin <125870016+SimonArduin@users.noreply.github.com>
2024-02-26 11:10:12 +0100

    added basic single user authentication

--- gateway/src/main/java/com/medilabo/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/medilabo/gateway/SecurityConfig.java
@@ -2,32 +2,40 @@ package com.medilabo.gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build()
-        );
+    public MapReactiveUserDetailsService userDetailsService() {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService (user);
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests( auth -> auth
-                .requestMatchers("/note/**").authenticated());
+    public SecurityWebFilterChain securityFilterChain (ServerHttpSecurity http) throws Exception {
+        http
+                .authorizeExchange((authorize) -> authorize
+                        .pathMatchers("/patient/**").permitAll()
+                        .pathMatchers("/note/**").authenticated()
+                        .anyExchange().denyAll()
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
         return http.build();
     }
 }
